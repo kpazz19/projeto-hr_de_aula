9c2d68d6217f5f40e1f954924901ea1699a77837
kpazz19 <kaupazz19@gmail.com>
2022-08-26 02:18:17 -0300

    apenas a estrutura
    ver se a sintaxe funciona!
    e escrever os dias ainda

--- projeto pikadastico.cpp
+++ projeto pikadastico.cpp
@@ -6,8 +6,54 @@
 
 int main()
 {
-	char [30];
-	int numero;
+	char nome [30];
+	int numero, dia;
+	
+	printf(" DOMINGO -> 0 \n");
+	printf("SEGUND-FEIRA -> 1 \n ");
+	printf(" TERCA-FEIRA -> 2 \n ");
+	printf(" QUARTA-FEIRA -> 3 \n ");
+	printf(" QUINTA-FEIRA -> 4 \n ");
+	printf(" SEXTA-FEIRA -> 5 \n ");
+	printf(" SABADO -> 6 \n ");
+	
+	
+	printf("me diga seu  mome: \n ");
+	scanf("%s", &nome);
+
+	printf(" digite o o numero do dia da semana:");
+	scanf("%i", &numero);
+	
+	if ( numero == 0)
+	
+	else if ( numero == 1 )
+	
+	else if ( numero == 2 )
+	
+	else if ( numero == 3 )
+	
+	else if ( numero == 4 )
+	
+	else if ( numero == 5 )
+	
+	else if ( numero == 6 )
+	
+	else if ( numero != 0, 1, 2, 3, 4, 5, 6 )
+	{
+		printf("\n digite entre 0 e 6 seu burro ");
+	}
+	
+	else 
+	{
+		printf("\n digitou errado, VOLTE PARA O ENSINO MEDIO ja que nao sabe ler ");
+	}
+	
+	
+	printf ("\n aqui esta seu cronograma", nome);
+	printf("cronograma : %i \n", numero);
+	
+	
+	
 	
 	
 
