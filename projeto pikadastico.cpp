1617ec8106b0e2e20e644307caccaa23848e8a9a
kpazz19 <111325470+kpazz19@users.noreply.github.com>
2022-08-26 10:06:33 -0300

    final do projeto
    finalizando o projeto para o horario 
    creditos by: luizinho e pazz

--- projeto pikadastico.cpp
+++ projeto pikadastico.cpp
@@ -7,54 +7,56 @@
 int main()
 {
 	char nome [30];
-	int numero, dia;
+	int numero;
 	
-	printf(" DOMINGO -> 0 \n");
-	printf("SEGUND-FEIRA -> 1 \n ");
-	printf(" TERCA-FEIRA -> 2 \n ");
-	printf(" QUARTA-FEIRA -> 3 \n ");
-	printf(" QUINTA-FEIRA -> 4 \n ");
-	printf(" SEXTA-FEIRA -> 5 \n ");
-	printf(" SABADO -> 6 \n ");
+	printf(" DOMINGO -> 1 \n");
+	printf("SEGUND-FEIRA -> 2 \n ");
+	printf(" TERCA-FEIRA -> 3 \n ");
+	printf(" QUARTA-FEIRA -> 4 \n ");
+	printf(" QUINTA-FEIRA -> 5 \n ");
+	printf(" SEXTA-FEIRA -> 6 \n ");
+	printf(" SABADO -> 7 \n ");
 	
 	
 	printf("me diga seu  mome: \n ");
 	scanf("%s", &nome);
 
-	printf(" digite o o numero do dia da semana:");
+	printf(" digite o o numero do dia da semana:\n ");
 	scanf("%i", &numero);
 	
-	if ( numero == 0)
-	
-	else if ( numero == 1 )
-	
-	else if ( numero == 2 )
-	
-	else if ( numero == 3 )
-	
-	else if ( numero == 4 )
-	
-	else if ( numero == 5 )
-	
-	else if ( numero == 6 )
-	
-	else if ( numero != 0, 1, 2, 3, 4, 5, 6 )
+	printf (" aqui esta sr(a) %s \n \n", nome);
+	if ( numero == 1 )
 	{
-		printf("\n digite entre 0 e 6 seu burro ");
+		printf(" dia do descanso.");
 	}
-	
+	else if ( numero == 2 )
+	{
+		printf(" Seu Cronograma do dia: \n * 7:00-9:15 --> Construcao de algoritmos - (Laboratorio C)\n\n * 9:15-9:30 --> Intervalo\n\n * 9:30-11:00 --> Administracao e Estrutura organizacional - (Laboratorio C) \n \n Boa aula ;) ");
+	}
+	else if ( numero == 3 )
+	{
+		printf(" Seu Cronograma do dia: \n * 7:00-8:30 --> Matematica aplicada - (Sala 14)\n\n * 8:30-9:15 --> Sistemas de informacao - (Laboratorio C)\n\n * 9:15-9:30 --> Intervalo\n\n * 9:30-10:15 --> Sistemas de informacao - (Laboratorio C) \n \n Boa aula ;) ");
+	}
+	else if ( numero == 4 )
+	{
+		printf(" Seu Cronograma do dia: \n * 7:00-8:30 --> Administracao e Estrutura organizacional - (Laboratorio C)\n\n * 8:30-9:15 --> Introducao a tecnologia da computacao - (Sala 14)\n\n * 9:15-9:30 -->intervalo\n\n * 9:30-10:15 --> Introducao a tecnologia da computacao - (Sala 14) \n \n Boa aula ;) ");
+	}
+	else if ( numero == 5 )
+	{
+		printf(" Seu Cronograma do dia: \n * 7:00-8:30 --> Matematica aplicada - (Sala 14)\n\n * 8:30-9:15 Introducao a tecnologia da computacao - (Sala 14) \n\n * 9:15-9:30 Inteveralo\n\n * 9:30-10:15 --> Introducao a tecnologia da computacao - (Sala 14) \n \n Boa aula ;) ");
+	}
+	else if ( numero == 6 )
+	{
+		printf(" Seu Cronograma do dia: \n * 7:00-9:15 --> Construcao de algoritmos - (Laboratorio C)\n\n * 9:15-9:30 --> Intervalo\n\n * 9:30-11:00 --> Ingles tecnico - (Laboratorio C) \n \n Boa aula ;) ");
+	}
+	else if ( numero == 7 )
+	{
+		printf("Dia de role baby");
+	}
 	else 
 	{
 		printf("\n digitou errado, VOLTE PARA O ENSINO MEDIO ja que nao sabe ler ");
 	}
 	
-	
-	printf ("\n aqui esta seu cronograma", nome);
-	printf("cronograma : %i \n", numero);
-	
-	
-	
-	
-	
+}	
 
-}
